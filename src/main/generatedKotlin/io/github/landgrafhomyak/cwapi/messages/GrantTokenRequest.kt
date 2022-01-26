0f9356858b8df9659cff9a73673797e727bc7215
Andrew Golovashevich <tankist.scratch.p@gmail.com>
2022-01-26 03:48:32 +0300

    Allow generated sources in release commits

---
+++ src/main/generatedKotlin/io/github/landgrafhomyak/cwapi/messages/GrantTokenRequest.kt
@@ -0,0 +1,6 @@
+package io.github.landgrafhomyak.cwapi.messages
+
+class GrantTokenRequest(
+   var authCode: io.github.landgrafhomyak.cwapi.types.AuthCode,
+   var userId: io.github.landgrafhomyak.cwapi.types.TelegramUserId,
+) : io.github.landgrafhomyak.cwapi.messages.Message.Client2Server
